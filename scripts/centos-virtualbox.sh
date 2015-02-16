69c126cb5e342386eb94c213e61924a1ca9da2d2
Mahyuddin Susanto <udienz@gmail.com>
2015-02-16 14:19:09 +0700

    Delete unused scripts, refreshed gitignore
    Signed-off-by: Mahyuddin Susanto <udienz@gmail.com>

--- scripts/centos-virtualbox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir /tmp/vbox
-mount -t iso9660 -o loop /home/vagrant/VBoxGuestAdditions.iso /tmp/vbox
-/tmp/vbox/VBoxLinuxAdditions.run
-umount /tmp/vbox
-rm -rf /tmp/vbox /home/vagrant/.vbox_version
